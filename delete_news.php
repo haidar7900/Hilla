a18acbd01d0b97d7ac33c5377141fa894327fe1d
haidar7900 <161032580+haidar7900@users.noreply.github.com>
2025-03-26 16:38:24 +0300

    delete_news.php

--- delete_news.php
+++ delete_news.php
@@ -1,6 +1,15 @@
 <?php
+session_start();
+if (!isset($_SESSION['admin'])) {
+    header("Location: admin_login.php");
+    exit();
+}
+
 include 'db.php';
 $id = $_GET['id'];
-$conn->query("DELETE FROM news WHERE id=$id");
+
+$sql = "DELETE FROM news WHERE id=$id";
+$conn->query($sql);
+
 header("Location: admin_dashboard.php");
 ?>
